f26c6a196fd445337c7cdd659f441df72e67d69b
Anton <anton.liepelt@gmx.de>
2024-12-04 19:19:43 +0100

    Initial commit

---
+++ src/test/java/com/hansablock/eventscraper/EventscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansablock.eventscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
