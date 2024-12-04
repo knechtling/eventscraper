8f95499eed5de937cb5d027eafeacc1100e6222b
Anton <anton.liepelt@gmx.de>
2024-12-04 21:58:06 +0100

    jw<aKOLihjdf IOAPuhei

--- src/main/java/com/hansablock/eventscraper/EventController.java
+++ src/main/java/com/hansablock/eventscraper/EventController.java
@@ -1,4 +1,19 @@
 package com.hansablock.eventscraper;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping("/")
+    public String showEvents(Model model) {
+        model.addAttribute("events", eventService.getEvents());
+        return "welcome";
+    }
 }
