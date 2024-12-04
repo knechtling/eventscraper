090500ce107f955f491a4be090306d9d3ca3ce49
Anton <anton.liepelt@gmx.de>
2024-12-04 20:33:07 +0100

    aeiokwijfoaöwij

--- src/main/java/com/hansablock/eventscraper/EventRepository.java
+++ src/main/java/com/hansablock/eventscraper/EventRepository.java
@@ -1,4 +1,11 @@
 package com.hansablock.eventscraper;
 
-public class EventRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    static List<Event> findByTitle(String title);
 }
