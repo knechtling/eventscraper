c09aa86a72928e7517e38a8ae77d0e83facb4be8
Anton <anton.liepelt@gmx.de>
2024-12-04 20:33:07 +0100

    aeiokwijfoaöwij

--- src/main/java/com/hansablock/eventscraper/Event.java
+++ src/main/java/com/hansablock/eventscraper/Event.java
@@ -1,4 +1,28 @@
 package com.hansablock.eventscraper;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Event {
+    @Id
+    private Long id;
+
+    private String title;
+
+
+
+
+
+
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
--- src/main/java/com/hansablock/eventscraper/EventService.java
+++ src/main/java/com/hansablock/eventscraper/EventService.java
@@ -1,4 +1,13 @@
 package com.hansablock.eventscraper;
 
+import java.util.List;
+
 public class EventService {
+    public String viewEvent() {
+        return "Event";
+    }
+
+    public List<Event> searchEvents(String title) {
+        return EventRepository.findByTitle(title);
+    }
 }
