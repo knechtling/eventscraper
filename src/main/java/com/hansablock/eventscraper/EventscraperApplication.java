163f7ccd002414b689e8113c4715dd2e044b7b36
Anton <anton.liepelt@gmx.de>
2024-12-04 19:19:43 +0100

    Initial commit

---
+++ src/main/java/com/hansablock/eventscraper/EventscraperApplication.java
@@ -0,0 +1,13 @@
+package com.hansablock.eventscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventscraperApplication.class, args);
+	}
+
+}
