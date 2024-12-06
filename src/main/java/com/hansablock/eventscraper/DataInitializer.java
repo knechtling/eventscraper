de6c345412b5f7f02bd2f6f5f7485157cbd56335
Anton <anton.liepelt@gmx.de>
2024-12-06 03:44:27 +0100

    implemented scrappy scraper package

--- src/main/java/com/hansablock/eventscraper/DataInitializer.java
+++ src/main/java/com/hansablock/eventscraper/DataInitializer.java
@@ -1,23 +1,17 @@
 package com.hansablock.eventscraper;
 
+import com.hansablock.eventscraper.scraper.ScraperService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Configuration
 public class DataInitializer {
 
     @Bean
-    CommandLineRunner initDatabase(EventRepository eventRepository) {
+    CommandLineRunner initDatabase(ScraperService scraperService) {
         return args -> {
-            eventRepository.save(new Event(null, "Event 1", "Location 1", LocalDate.of(2024, 1, 1), "Heavy Metal", LocalTime.of(19, 0), LocalTime.of(20, 0), new BigDecimal("10.00")));
-            eventRepository.save(new Event(null, "Event 2", "Location 2", LocalDate.of(2025, 1, 2), "Heavy Blouse", LocalTime.of(19, 0), LocalTime.of(20, 0), new BigDecimal("10.00")));
-            eventRepository.save(new Event(null, "Event 3", "Location 3", LocalDate.of(2026, 1, 3), "Rock", LocalTime.of(19, 0), LocalTime.of(20, 0), new BigDecimal("10.00")));
-            eventRepository.save(new Event(null, "Event 4", "Location 4", LocalDate.of(2027, 1, 4), "Iii Pop", LocalTime.of(19, 0), LocalTime.of(20, 0), new BigDecimal("10.00")));
+            scraperService.scrapeAndSaveEvents();
         };
     }
 }
--- src/main/java/com/hansablock/eventscraper/EventController.java
+++ src/main/java/com/hansablock/eventscraper/EventController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class EventController {
@@ -11,9 +14,18 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
+    @Autowired
+    private EventRepository eventRepository;
+
     @GetMapping("/")
-    public String showEvents(Model model) {
-        model.addAttribute("events", eventService.getEvents());
+    public String showEvents(@RequestParam(value = "search", required = false) String search, Model model) {
+        List<Event> events;
+        if (search != null && !search.isEmpty()) {
+            events = eventRepository.findByTitleContainingIgnoreCase(search);
+        } else {
+            events = eventService.getEvents();
+        }
+        model.addAttribute("events", events);
         return "welcome";
     }
 }
