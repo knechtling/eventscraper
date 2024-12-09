476d00da64e147b8fc948eff3d6206a9e163d7dd
Anton <anton.liepelt@gmx.de>
2024-12-09 15:58:20 +0100

    make everything ready for build jar and docker

--- src/main/java/com/hansablock/eventscraper/EventRepository.java
+++ src/main/java/com/hansablock/eventscraper/EventRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByTitleContainingIgnoreCase(String title);
     List<Event> findAllByOrderByDateAsc();
+    boolean existsByDescription(String description);
 }
--- src/main/java/com/hansablock/eventscraper/EventscraperApplication.java
+++ src/main/java/com/hansablock/eventscraper/EventscraperApplication.java
@@ -2,8 +2,10 @@ package com.hansablock.eventscraper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EventscraperApplication {
 
 	public static void main(String[] args) {
